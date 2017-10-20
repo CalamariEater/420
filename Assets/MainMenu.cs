16a73fe266577e0715635d61728a207f2e35eebb
Meep7 <32401566+Meep7@users.noreply.github.com>
2017-10-20 15:56:10 -0700

    Main_menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour {
+
+	public void NGButton(string newGameLevel){
+		SceneManager.LoadScene(newGameLevel);
+	}
+
+	public void ExitButton(){
+		Application.Quit();
+	}
+
+}
