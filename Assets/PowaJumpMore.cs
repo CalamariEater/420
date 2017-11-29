0adb3136e5fa20eaebb70810ea62930e71cb6545
asleepu <leej254@unlv.nevada.edu>
2017-11-29 10:17:37 -0800

    reimplemented kevin's POWAJUMPMORE
    tentative powerup added

--- Assets/PowaJumpMore.cs
+++ Assets/PowaJumpMore.cs
@@ -4,12 +4,16 @@ using System.Collections;
 public class PowaJumpMore : MonoBehaviour {
 
 	private int LayerPlayer;
+    private GameObject thePlayer;
+    private Controller playerScript; // To change any values on player
+    public int jumpLimit = 2;
 
-
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		LayerPlayer = LayerMask.NameToLayer("Player");
-	}
+        thePlayer = GameObject.Find("Player");
+        playerScript = thePlayer.GetComponent<Controller>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -19,7 +23,8 @@ public class PowaJumpMore : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.tag == "Player") {
 			Debug.Log ("POWA UUUUUP");
-
+            Destroy(gameObject);
+            playerScript.jumpLimit += 1;
 		}
 	}
 }
