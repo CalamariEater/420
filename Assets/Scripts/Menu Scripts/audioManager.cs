a1154d64beadb067e24074df0fb2bb4b90693520
Erik Perez-Martinez <perezmar@u-10-36-5-240.dhcp.unlv.edu>
2017-11-30 15:47:16 -0800

    audio options

---
+++ Assets/Scripts/Menu Scripts/audioManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class audioManager : MonoBehaviour {
+
+	public AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+		audio.volume = PlayerPrefs.GetFloat ("MasterVol");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		audio.volume = PlayerPrefs.GetFloat ("MasterVol");
+	}
+}
--- Assets/Scripts/Menu Scripts/optionsManager.cs
+++ Assets/Scripts/Menu Scripts/optionsManager.cs
@@ -2,24 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class optionsManager : MonoBehaviour {
+
+
+	public Slider volumeSlider;
+	public AudioSource myMusic;
+
 
 	// Use this for initialization
 	void Start () {
 		//Time.timeScale = 1; //starts time should start game.
+		volumeSlider.value = PlayerPrefs.GetFloat ("MasterVol");
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		myMusic.volume = volumeSlider.value;
 	}
 
 	public void DoneBtn(){
 		//int previousLevel = PlayerPrefs.GetInt ("previousLevel");
 		//Application.LoadLevel (previousLevel);
+		PlayerPrefs.SetFloat("MasterVol", myMusic.volume);
 		SceneManager.UnloadScene ("Options");
 
 	}
+
+
 }
