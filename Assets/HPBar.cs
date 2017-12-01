7f442e8dd4386841a8b5991d2aeca3198ff6ba58
Kevin John Bulosan <bulosan@u-10-36-5-196.dhcp.unlv.edu>
2017-11-30 18:04:37 -0800

    Added Health
    - not added to every scene atm

---
+++ Assets/HPBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBar : MonoBehaviour {
+
+	private GameObject thePlayer;
+	private Controller playerScript; // To change any values on player
+	public Slider theSlider; // access to the slider
+
+	private float previousHP;
+
+	// Use this for initialization
+	void Start () {
+		thePlayer = GameObject.Find ("Player");
+		playerScript = thePlayer.GetComponent<Controller> ();
+		previousHP = playerScript.hp; // get initial hp val
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (previousHP != playerScript.hp) {
+			theSlider.value = playerScript.hp;
+			previousHP = theSlider.value;
+		}
+	}
+}
