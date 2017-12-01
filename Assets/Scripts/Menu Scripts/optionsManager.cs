6138a2ff0e5898b69d37a9e4bf6b179e3e29ba94
Erik Perez-Martinez <perezmar@u-10-36-5-240.dhcp.unlv.edu>
2017-11-30 16:35:07 -0800

    a

--- Assets/Scripts/Menu Scripts/optionsManager.cs
+++ Assets/Scripts/Menu Scripts/optionsManager.cs
@@ -9,24 +9,25 @@ public class optionsManager : MonoBehaviour {
 
 	public Slider volumeSlider;
 	public AudioSource myMusic;
-
+	public Text volumeText;
 
 	// Use this for initialization
 	void Start () {
 		//Time.timeScale = 1; //starts time should start game.
 		volumeSlider.value = PlayerPrefs.GetFloat ("MasterVol");
-
+		volumeText.text = "" + Mathf.Round (100 * volumeSlider.value);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		PlayerPrefs.SetFloat("MasterVol", myMusic.volume);
+		volumeText.text = "" + Mathf.Round (100 * volumeSlider.value);
 		myMusic.volume = volumeSlider.value;
 	}
 
 	public void DoneBtn(){
 		//int previousLevel = PlayerPrefs.GetInt ("previousLevel");
 		//Application.LoadLevel (previousLevel);
-		PlayerPrefs.SetFloat("MasterVol", myMusic.volume);
 		SceneManager.UnloadScene ("Options");
 
 	}
