ebd0972b99bd74725acf68b1d726e0872a78ed52
CalamariEater <itskjbulosan@gmail.com>
2017-12-01 18:17:37 -0800

    working on timer
    - its still not done

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public Text timerText;
+
+	// Use this for initialization
+	void Start () {
+        timerText.text = "Text: " + PlayerPrefs.GetFloat("currTime");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timerText.text = "Text: " + PlayerPrefs.GetFloat("currTime");
+    }
+}
